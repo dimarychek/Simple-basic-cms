bb9f24c6b04083d50d6c378df6818d03ff360506
rychek <d.rychek@lestro.com>
2016-11-03 22:49:06 +0200

    first edit

--- article.php
+++ article.php
@@ -4,6 +4,6 @@ require_once('database.php');
 require_once('models/functions.php');
 
 $link = db_connect();
-$article = articles_get($link, $_GET['id']);
+//$article = articles_get($link, $_GET['id']);
 
 include('views/site/article.php');
